ecb5534bf9438d67b83c8a71e19f4131effe3189
Douglas Souza <douglassouzabjj@hotmail.com>
2021-12-21 18:32:26 -0300

    estiliza componente ValueFilter

--- src/components/ValueFilter.jsx
+++ src/components/ValueFilter.jsx
@@ -28,33 +28,45 @@ const ValueFilter = () => {
 
   return (
     <form>
-      <select
-        data-testid="column-filter"
-        onChange={ ({ target }) => setColumn(target.value) }
-      >
-        { renderOptions() }
-      </select>
-      <select
-        data-testid="comparison-filter"
-        onChange={ ({ target }) => setComparison(target.value) }
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        data-testid="value-filter"
-        type="number"
-        onChange={ ({ target }) => setValue(target.value) }
-      />
-      <Button
-        variant="dark"
-        type="button"
-        data-testid="button-filter"
-        onClick={ () => addNumericFilter({ column, comparison, value }) }
-      >
-        Filtrar
-      </Button>
+      <div className="row g-3">
+        <div className="col-auto">
+          <select
+            className="form-select"
+            data-testid="column-filter"
+            onChange={ ({ target }) => setColumn(target.value) }
+          >
+            { renderOptions() }
+          </select>
+        </div>
+        <div className="col-auto">
+          <select
+            className="form-select col-auto"
+            data-testid="comparison-filter"
+            onChange={ ({ target }) => setComparison(target.value) }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </div>
+        <div className="col-auto">
+          <input
+            data-testid="value-filter"
+            type="number"
+            onChange={ ({ target }) => setValue(target.value) }
+          />
+        </div>
+        <div className="col-auto">
+          <Button
+            variant="dark"
+            type="button"
+            data-testid="button-filter"
+            onClick={ () => addNumericFilter({ column, comparison, value }) }
+          >
+            Filtrar
+          </Button>
+        </div>
+      </div>
     </form>
   );
 };
